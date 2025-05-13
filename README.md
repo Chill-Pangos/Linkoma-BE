cf52e13b83be4d1ba2a37c379a469abb12d0db6a
Minh Tuấn <75988895+MinhTuan120704@users.noreply.github.com>
2025-05-13 13:33:14 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Linkoma-BE
+ Linkoma-BE is the backend for a mini apartment management system, providing features for managing residents, apartments, parking spaces, invoices, service fees, and resident requests/feedback. It includes role-based access control, security, reporting, and notifications, ensuring efficient and secure management of the apartment complex.
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const xss = require("xss-clean");
+const compression = require("compression");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+
+//enable cors
+app.use(cors());
+app.options("*", cors());
+
+//set security HTTP headers
+app.use(helmet());
+
+//parse json request body
+app.use(express.json());
+
+//parse urlencoded request body
+app.use(express.urlencoded({ extended: true}))
+
+//set HTTP requests logger
+app.use(morgan("dev"));
+
+//set xss clean
+app.use(xss());
+
+//compress response bodies
+app.use(compression());
+
+module.exports = app;
