a428d129bb6539c52e60be3f9584508cd829ab95
Minh Tuấn <75988895+MinhTuan120704@users.noreply.github.com>
2025-05-21 11:05:12 +0700

    chore(config): update fieldConfig/index.js

--- src/config/fieldConfig/index.js
+++ src/config/fieldConfig/index.js
@@ -1,3 +1,12 @@
 module.exports = {
     user: reqruire('./user.fieldconfig'),
+    announcement: require('./announcement.fieldconfig'),
+    apartment: require('./apartment.fieldconfig'),
+    apartmentType: require('./apartmentType.fieldconfig'),
+    contract: require('./contract.fieldconfig'),
+    feedback: require('./feedback.fieldconfig'),
+    invoice: require('./invoice.fieldconfig'),
+    invoiceDetail: require('./invoiceDetail.fieldconfig'),
+    serviceRegistration: require('./serviceRegistration.fieldconfig'),
+    serviceType: require('./serviceType.fieldconfig'),
 };
